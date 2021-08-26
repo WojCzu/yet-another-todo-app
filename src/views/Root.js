622946bb8a53664f3758f75ffcef3f84aebdc46c
WojCzu <wojciech.czuma@gmail.com>
2021-08-26 12:50:53 +0200

    add ThemeProvider with empty theme

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'assets/styles/theme';
 
 const Root = () => (
-  <>
+  <ThemeProvider theme={theme}>
     <GlobalStyle />
     <h1>Hello</h1>
-  </>
+  </ThemeProvider>
 );
 
 export default Root;
