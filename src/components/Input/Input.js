5fb1fb3f5ee99062d0d245d5c149fadf36a7ab07
WojCzu <wojciech.czuma@gmail.com>
2021-09-01 23:40:14 +0200

    introduce reducer - add task

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'components/Button/Button';
 import plus from 'assets/icons/plus.svg';
 import { StyledForm, StyledInput } from './Input.styles';
 
-const Input = () => {
+const Input = ({ addUser }) => {
+  const [name, setName] = useState('');
   return (
-    <StyledForm>
-      <StyledInput type="text" placeholder="Create a new task..." />
+    <StyledForm
+      onSubmit={(e) => {
+        e.preventDefault();
+        addUser(name);
+        setName('');
+      }}
+    >
+      <StyledInput
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        type="text"
+        placeholder="Create a new task..."
+      />
       <Button icon={plus} type="submit" />
     </StyledForm>
   );
--- src/views/App.js
+++ src/views/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import TaskList from 'components/TaskList/TaskList';
 import NavBar from 'components/NavBar/NavBar';
 import { Wrapper, ListWrapper, Title } from './App.styles';
-import data from 'data/tasks';
+import tasks from 'data/tasks';
 import Input from 'components/Input/Input';
+import { v4 as uuid } from 'uuid';
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_TASK':
+      return [...state, action.payload];
+    case 'DEL_TASK':
+      return state;
+    case 'FINISH_TASK':
+      return state;
+
+    default:
+      return state;
+  }
+};
 
 const App = () => {
+  const [data, dispatch] = useReducer(reducer, tasks);
   const tasksLeft = data.reduce(
     (sum, task) => (task.isFinished ? sum + 1 : sum),
     0
@@ -15,7 +31,14 @@ const App = () => {
       <Title>
         Yet another <span>ToDo app</span>
       </Title>
-      <Input />
+      <Input
+        addUser={(name) =>
+          dispatch({
+            type: 'ADD_TASK',
+            payload: { id: uuid(), name, isFinished: false },
+          })
+        }
+      />
       <ListWrapper>
         <TaskList data={data} />
         <NavBar tasksLeft={tasksLeft} />
